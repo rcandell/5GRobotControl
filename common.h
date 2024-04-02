b283ae3f1c7ab341565963e231d4c501530b787e
Rick Candell <rick.candell@gmail.com>
2024-04-02 15:40:34 -0400

    Create common.h

---
+++ common.h
@@ -0,0 +1,13 @@
+
+typedef struct {
+  float v_left;
+  float v_right;
+  float v_up;
+  float v_down;
+  float v_fwd;
+  float v_bwd;
+  float thetadot_left;
+  float thetadot_right;
+  float thetadot_up;
+  float thetadot_down;
+} VelocityControllerMessage;
